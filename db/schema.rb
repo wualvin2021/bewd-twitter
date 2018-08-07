ccd9c9eb38d1e49bd68beb091b79f61162782559
Harry <harrychenca@gmail.com>
2018-08-07 11:03:17 +0800

    Update gemfile and clear db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217064119) do
+ActiveRecord::Schema.define(version: 0) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "sessions", force: :cascade do |t|
-    t.string   "token"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id", using: :btree
-  end
-
-  create_table "tweets", force: :cascade do |t|
-    t.string   "message"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tweets_on_user_id", using: :btree
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
-  end
-
-  add_foreign_key "sessions", "users"
-  add_foreign_key "tweets", "users"
 end
